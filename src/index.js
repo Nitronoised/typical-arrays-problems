29acf2d381a469c68014cc096b9157a8431e52fc
Yaseen <al.hashimi.yaseen@hotmail.com>
2021-02-14 00:17:45 +0300

    init : submit solution for typical-array-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array === null || array === [] || array.length < 1) { return 0 }
+  if (array.length > 1) { return Math.min(...array); }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array === null || array === [] || array.length < 1) { return 0 }
+  if (array.length > 1) { return Math.max(...array); }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array === null || array === [] || array.length < 1) { return 0 }
+  if (array.length > 1) { return (array.reduce((a, b) => a + b)) / array.length; }
 }
+
